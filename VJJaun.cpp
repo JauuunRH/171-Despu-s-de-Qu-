3ac43ce35c7a4c0002a230d798bfc4f2aa9fc4fd
Jauuuun <126738246+JauuunRH@users.noreply.github.com>
2023-05-19 01:38:48 -0600

    Add files via upload

---
+++ VJJaun.cpp
@@ -0,0 +1,11 @@
+#include<string.h>
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<windows.h>
+#include<time.h>
+
+using namespace std;
+
+int vidas=3, bloq, gameover=0, permisos1=0, permisos2=0, permisos3=0, permisos4=0;
